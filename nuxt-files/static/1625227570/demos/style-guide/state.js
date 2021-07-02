63a3f0cdec2f172ef29e4dfe8d423ea41d7f72dc
robinbortlik <robinbortlik@users.noreply.github.com>
2021-07-02 12:17:18 +0000

    deploy: c646e3a64aa221785fbe93ea0c08129759859dd8

--- nuxt-files/static/1625227570/demos/style-guide/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdaisyui\u002Fnuxt-files\u002Fstatic\u002F1625227570",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdemos\u002Fstyle-guide",config:{_app:{basePath:"\u002Fdaisyui\u002F",assetsPath:"\u002Fdaisyui\u002Fnuxt-files\u002F",cdnURL:a}}}}(null));
---
+++ nuxt-files/static/1625228231/components/progress/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdaisyui\u002Fnuxt-files\u002Fstatic\u002F1625228231",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcomponents\u002Fprogress",config:{_app:{basePath:"\u002Fdaisyui\u002F",assetsPath:"\u002Fdaisyui\u002Fnuxt-files\u002F",cdnURL:a}}}}(null));
